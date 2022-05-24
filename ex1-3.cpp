d053940002b27ce60bd80243b931dbe192fbd928
DoraChang <104436657+DoraChang@users.noreply.github.com>
2022-05-24 22:04:22 +0800

    ex1-3.cpp

---
+++ ex1-3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i,n;
+    for(i=0;i<n;i++){
+        for(j=0;j<=9;j++){
+          printf("%2d %2d\n",i,j);
+    }
+}
+    cout<<"Hello World";
+
+    return 0;
+}
