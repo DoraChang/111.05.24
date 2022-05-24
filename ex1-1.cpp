90faaf3a80d53d30077d65de838c4ba361dc6527
DoraChang <104436657+DoraChang@users.noreply.github.com>
2022-05-24 20:31:09 +0800

    ex1-1.cpp
    帶數字進去

---
+++ ex1-1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a[6]={8,3,6,10,45,12}; // int a0,a1,a2,a3,a4,a5
+    int b[6]={0};
+    //char s[]="this is a book":
+    int i,n;
+    
+    n=sizeof(a)/sizeof(int);
+    for(i=0;i<n;i++){
+      printf("a[%d]=%d\n",i,a[i]);
+    }
+    
+    n=sizeof(b)/sizeof(int);
+    for(i=0;i<n;i++){
+      printf("b[%d]=%d\n",i,b[i]);
+    }
+    cout<<"Hello World";
+    
+    return 0;
+}
+
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a[6]={8,3,6,10,45,12}; // int a0,a1,a2,a3,a4,a5
+    int b[6]={0};
+    //char s[]="this is a book":
+    int i,n;
+    
+    n=sizeof(a)/sizeof(int);
+    for(i=0;i<n;i++){
+        printf("a[%d]=%d\n",i,a[i]);
+    }
+    for()
+      scanf("%d",&b[i]);
+      
+    for()
+      printf b[i]
+      
+    cout<<"Hello World";
+    
+    return 0;
+}
